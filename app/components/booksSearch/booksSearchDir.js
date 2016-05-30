7fa52833b17605aa85e37099bd33034835787a66
Javier Anselmi <javieranselmi@gmail.com>
2016-05-30 17:55:21 -0300

    Agregado el books edit

--- app/components/booksSearch/booksSearchDir.js
+++ app/components/booksSearch/booksSearchDir.js
@@ -1,13 +1,23 @@
-app.directive('searchBooks', function() {
+app.directive('searchBooks', ["$timeout","$location","$http", function($timeout,$location,$http) {
   return {
     restrict: 'E',
     templateUrl: 'app/components/booksSearch/booksSearchDir.html',
     scope: {
         books: "="
     },
-    link: function() {
+    link: function(scope) {
 
-        //$('.tooltip').tooltip(options)
+        scope.deleteStatus = "unset";
+        scope.edit = function ( path ) {
+            $location.path( path );
+        };
+        scope.delete = function ( book ) {
+            $http.delete("/api/books/"+ book.id).then(function() {
+                scope.deleteStatus = "success";
+            },function(){
+                scope.deleteStatus = "error";
+            });
+        };
     }
   };
-});
+}]);
