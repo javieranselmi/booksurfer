f6242d9531690278c1a49a3d5dd981f74a4e337a
javieranselmi <javieranselmi@gmail.com>
2016-12-02 11:23:21 -0300

    Added books list to loan home

--- app/components/loan-manager/controllers/loan-manager.home.controller.js
+++ app/components/loan-manager/controllers/loan-manager.home.controller.js
@@ -2,9 +2,9 @@
 (function() {
     var moduleName     = 'loan-manager',
         controllerName = 'loanManagerHomeController';
-    controller.$inject = ['$scope'];
+    controller.$inject = ['$scope','$stateParams','entityAbm','entitySearch','$http','endpoints','$filter','$uibModal','$state'];
 
-    function controller($scope) {
+    function controller($scope, $stateParams, entityAbm, entitySearch, $http, endpoints, $filter, $uibModal, $state) {
         $scope.showBooks = true;
         $scope.showSamples = false;
 
@@ -17,6 +17,33 @@
             $scope.showBooks = true;
             $scope.showSamples = false;
         }
+
+        entitySearch.getAllEntities('members').then(function(result){
+            $scope.allMembers = result.data;
+        });
+        entitySearch.getAllEntities('samples').then(function(result){
+            $scope.allSamples = result.data;
+        });
+        entitySearch.getAllEntities('books').then(function(result){
+            $scope.allBooks = result.data;
+        });
+
+        $scope.availableSamples = function(book) {
+            return book.samples.filter(function(sample) {return sample.availableForLoan === "True"}).length;
+        }
+
+        $scope.setBook = function(book) {
+            $scope.book = book;
+        }
+
+        $scope.unsetBook = function() {
+            $scope.book = undefined;
+        }
+
+        $scope.setSample = function(sample) {
+            $scope.sample = sample;
+        }
+
 
     }
     angular.module(moduleName).controller(controllerName, controller);
